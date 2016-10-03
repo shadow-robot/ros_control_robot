c7553c7a6fad98c2c1e84649b141248847742fcf
Toni Oliver <toni@shadowrobot.com>
2016-10-03 13:12:37 +0100

    Fix date

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
  *  Copyright (c) 2008, Willow Garage, Inc.
  *  All rights reserved.
  *
- *  Modified 2014, by Shadow Robot Company Ltd.
+ *  Modified 2016, by Shadow Robot Company Ltd.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
